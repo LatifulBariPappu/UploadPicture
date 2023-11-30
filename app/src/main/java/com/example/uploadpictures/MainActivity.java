dd4bf15f00eeb684e37d4da3dbfec1c46de9e052
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-11-30 17:59:10 +0600

    button integration

--- app/src/main/java/com/example/uploadpictures/MainActivity.java
+++ app/src/main/java/com/example/uploadpictures/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.uploadpictures;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgGallery=findViewById(R.id.imgGallery);
+        Button btnGallery=findViewById(R.id.btnGallery);
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
