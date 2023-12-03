3eb2bfa1adf2b807f7257ecae02adc92aec508d0
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-12-03 19:55:19 +0600

    gallery integration

--- app/src/main/java/com/example/uploadpictures/MainActivity.java
+++ app/src/main/java/com/example/uploadpictures/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.uploadpictures;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int GALLERY_REQ_CODE=1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +23,20 @@ public class MainActivity extends AppCompatActivity {
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent iGallery=new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery,GALLERY_REQ_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RESULT_OK){
+            if(requestCode==GALLERY_REQ_CODE){
+
+            }
+        }
     }
 }
